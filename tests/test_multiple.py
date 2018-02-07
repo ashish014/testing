94e5f81d417767bdd31d2fac1fabab217da21b46
ashish014 <36195551+ashish014@users.noreply.github.com>
2018-02-07 18:42:42 +0530

    update values

--- tests/test_multiple.py
+++ tests/test_multiple.py
@@ -1,5 +1,5 @@
 from maths import multiple, add
-def test_multiple_3_5():
-    assert multiple(3,5) == 15
-def test_add_3_5():
-    assert add(3,5) == 8
+def test_multiple_3_6():
+    assert multiple(3,6) == 18
+def test_add_3_6():
+    assert add(3,6) == 9
